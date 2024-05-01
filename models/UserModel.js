be4fbf0a9f52423fe00975fcd8155149f780e2e0
Akash Srivastava <akasriva@adobe.com>
2024-05-01 20:51:48 +0530

    profile pic added

--- models/UserModel.js
+++ models/UserModel.js
@@ -17,7 +17,11 @@ const userSchema = new mongoose.Schema({
   likes: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Story' // Referencing the Story model
-  }]
+  }],
+  profilePic: {
+    type: String,
+    default: "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg"
+  }
 });
 
 const User = mongoose.model('User', userSchema);
