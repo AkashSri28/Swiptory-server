54a11dc66a65f344ea66096a16ce416a14a1badb
Akash Srivastava <akasriva@adobe.com>
2024-05-05 13:51:12 +0530

    checkbookmark and checklike

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -6,6 +6,7 @@ const authMiddleware = async (req, res, next) => {
   const token = req.header('Authorization').replace('Bearer ', '');
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded)
     const user = await User.findOne({ _id: decoded.userId });
     if (!user) {
       throw new Error();
