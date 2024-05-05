3e3b379501be89e87a4cb4db84a026673ec7c68d
Akash Srivastava <akasriva@adobe.com>
2024-05-05 12:24:07 +0530

    backend error message fixed

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,7 +9,7 @@ const registerUser = async(req, res)=>{
         // Check if user already exists
         const existingUser = await User.findOne({ username });
         if (existingUser) {
-          return res.status(400).json({ message: 'User already exists' });
+          return res.status(201).json({ message: 'User already exists' });
         }
         // Hash the password
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -20,7 +20,7 @@ const registerUser = async(req, res)=>{
         // Generate JWT token
         const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, { expiresIn: '3d' });
 
-        res.status(201).json({ message: 'User registered successfully', user: newUser, token: token });
+        res.status(200).json({ message: 'User registered successfully', user: newUser, token: token });
     } catch (error) {
         console.error('Error registering user:', error);
         res.status(500).json({ message: 'Internal server error' });
